2ed8703d17a2ff4f12b168e22fa5ce3ca4aab08c
ddaawwaa <nlghtnmnt3106@gmail.com>
2019-11-13 01:23:56 +0900

    Create categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_161833) do
+ActiveRecord::Schema.define(version: 2019_11_12_162204) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
