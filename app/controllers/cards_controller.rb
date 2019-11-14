d8784792efe6947e938d2f0499655085401bcb70
kanato4 <kntykt@icloud.com>
2019-11-14 23:09:22 +0900

    create routing
    ルーティングの設定

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,22 @@
+class CardsController < ApplicationController
+  # layout "second_application"
+  # require "date"
+  # require "payjp"
+
+  def index
+    # card = current_user.credit_card
+    # redirect_to action: "show" unless card.blank?
+  end
+  def new
+    # card = current_user.credit_card
+    # redirect_to action: "show" unless card.blank?
+
+    # now_day = Date.today
+    # now_year = now_day.year % 100
+    # @year_array = []
+    # 11.times do
+    #   @year_array << now_year
+    #   now_year += 1
+    # end
+  end
+end
