138edbc96bc1e2d25f117ccf8f2768714fe6b44f
ddaawwaa <nlghtnmnt3106@gmail.com>
2019-11-17 19:37:16 +0900

    Set relation to active_hash

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,7 @@ class Item < ApplicationRecord
   enum postage_burden: { including_postage: 0, cash_on_delivery: 1 }
   enum seinding_methid: { undecided: 0, mercari_bin: 1, yu_mail: 2, letter_pack: 3, post: 4, kuroneko: 5, yu_pack: 6, click_post: 7, yu_packet: 8 }
   enum shipping_date: { within_two_days: 0, within_three_days: 1, within_seven_days: 2 }
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :pref
 end
