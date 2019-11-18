5f222d2f496dc29a659c108a0a97eec7d67d4295
Yoshi Tadokoro <yossher@gmail.com>
2019-11-18 12:50:57 +0900

    seedsファイル編集とdb以下にseedフォルダー追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# カテゴリのレコード追加
+require './db/seeds/area.rb'
+require './db/seeds/brand.rb'
+require './db/seeds/category.rb'
+require './db/seeds/condition.rb'
+require './db/seeds/sending_method.rb'
+require './db/seeds/size.rb'
