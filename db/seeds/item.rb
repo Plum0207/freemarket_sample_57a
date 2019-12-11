491ad2e463d6992032b8fcf4989b860503ee736a
Yoshi Tadokoro <yossher@gmail.com>
2019-12-11 22:31:32 +0900

    user, item, imageの各ファイルを作成、brandファイル編集

---
+++ db/seeds/item.rb
@@ -0,0 +1,35 @@
+# レディース （カテゴリー: その他）
+# Item.create!(id: 1, seller_id: 2, name: "レディース物", description: "[激レア] 良品です!!", category_id: 32, size: 0, brand_id: 0, condition: 0, postage_burden: 0, sending_method: 0, prefecture_from: 0, shipping_date: 0, price: 999)
+
+
+10.times do |number|
+    Item.create!(id: "#{number+1}", seller_id: 2, name: "レディース", description: "[激レア] 良品です!!", category_id: 32, size: "M", brand_id: 8, condition: 0, postage_burden: 0, sending_method: 0, prefecture_from: 0, shipping_date: 0, price: 999)
+  end
+
+10.times do |number|
+  Item.create!(id: "#{number+11}", seller_id: 1, name: "メンズ", description: "[激レア] 良品です!!", category_id: 229, size: 0, brand_id: 8, condition: 0, postage_burden: 0, sending_method: 0, prefecture_from: 0, shipping_date: 0, price: 999)
+end
+
+10.times do |number|
+  Item.create!(id: "#{number+21}", seller_id: 1, name: "家電・スマホ・カメラ", description: "[激レア] 良品です!!", category_id: 375, size: 0, brand_id: 8, condition: 0, postage_burden: 0, sending_method: 0, prefecture_from: 0, shipping_date: 0, price: 999)
+end
+
+10.times do |number|
+  Item.create!(id: "#{number+31}", seller_id: 1, name: "おもちゃ・ホビー・グッズ", description: "[激レア] 良品です!!", category_id: 506, size: 0, brand_id: 8, condition: 0, postage_burden: 0, sending_method: 0, prefecture_from: 0, shipping_date: 0, price: 999)
+end
+  
+10.times do |number|
+    Item.create!(id: "#{number+41}", seller_id: 2, name: "シャネル", description: "[激レア] 良品です!!", category_id: 32, size: 0, brand_id: 1, condition: 0, postage_burden: 0, sending_method: 0, prefecture_from: 0, shipping_date: 0, price: 999)
+end
+  
+10.times do |number|
+    Item.create!(id: "#{number+51}", seller_id: 2, name: "ルイ・ビトン", description: "[激レア] 良品です!!", category_id: 32, size: 0, brand_id: 3, condition: 0, postage_burden: 0, sending_method: 0, prefecture_from: 0, shipping_date: 0, price: 999)
+end
+
+10.times do |number|
+  Item.create!(id: "#{number+61}", seller_id: 1, name: "シュプリーム", description: "[激レア] 良品です!!", category_id: 229, size: 0, brand_id: 4, condition: 0, postage_burden: 0, sending_method: 0, prefecture_from: 0, shipping_date: 0, price: 999)
+end
+
+10.times do |number|
+  Item.create!(id: "#{number+71}", seller_id: 1, name: "ナイキ", description: "[激レア] 良品です!!", category_id: 229, size: 0, brand_id: 6, condition: 0, postage_burden: 0, sending_method: 0, prefecture_from: 0, shipping_date: 0, price: 999)
+end
---
+++ db/seeds/user.rb
@@ -0,0 +1,2 @@
+User.create!(id: 1, email: "tesuo1@testmail.com", password: "aaaaaa", password_confirmation: "aaaaaa", nickname: "テストくん１", first_name: "テス夫１", last_name: "テスト", first_name_kana: "てすおいち", last_name_kana: "てすと", birthday: 0000-00-00)
+User.create!(id: 2, email: "tesuko1@testmail.com", password: "aaaaaa", password_confirmation: "aaaaaa", nickname: "テストさん１", first_name: "テス子１", last_name: "テスト", first_name_kana: "てすこいち", last_name_kana: "てすと", birthday: 0000-00-00)
