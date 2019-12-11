9b5f0a369fe70d92a296a923d68498817134adbe
Yoshi Tadokoro <yossher@gmail.com>
2019-12-11 22:28:15 +0900

    seedファイルを編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require './db/seeds/brand.rb'
+
+# ① まずはこのまま"rake db:seed"を実行することで、ローカルのDBに以下user, category, brandのデータが入力されます。
+require './db/seeds/user.rb'
 require './db/seeds/category.rb'
+require './db/seeds/brand.rb'
+
+# ② 次に上記3行をコメントアウトし下記の行のコメントアウトを外して"rake db:seed"を実行することでitemのデータがローカルのDBに入力されます。
+# require './db/seeds/item.rb'
+
+# ③ 最後に上記itemの行をコメントアウトし下記の行のコメントアウトを外して"rake db:seed"を実行することで、各itemに紐づけられたimageのデータがローカルのDBに入力されます。
+# require './db/seeds/image.rb'
+
+# ローカルのDBのデータを全て消して一からやり直したい場合は、"rake db:migrate:reset"を実行してください。
