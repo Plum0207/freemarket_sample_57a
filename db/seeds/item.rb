94964ef92c4b393bdc736e5089e538e5fba5e210
Yoshi Tadokoro <yossher@gmail.com>
2019-12-12 13:35:39 +0900

    imageファイルの修正

--- db/seeds/item.rb
+++ db/seeds/item.rb
@@ -7,11 +7,11 @@
 end
 
 10.times do |number|
-  Item.create!(id: "#{number+21}", seller_id: 1, name: "家電・スマホ・カメラ", description: "[激レア] 良品です!!", category_id: 375, size: 0, brand_id: 8, condition: 0, postage_burden: 0, sending_method: 0, prefecture_from: 0, shipping_date: 0, price: 999)
+  Item.create!(id: "#{number+21}", seller_id: 1, name: "家電・スマホ・カメラ", description: "[激レア] 良品です!!", category_id: 920, size: 0, brand_id: 8, condition: 0, postage_burden: 0, sending_method: 0, prefecture_from: 0, shipping_date: 0, price: 999)
 end
 
 10.times do |number|
-  Item.create!(id: "#{number+31}", seller_id: 1, name: "おもちゃ・ホビー・グッズ", description: "[激レア] 良品です!!", category_id: 506, size: 0, brand_id: 8, condition: 0, postage_burden: 0, sending_method: 0, prefecture_from: 0, shipping_date: 0, price: 999)
+  Item.create!(id: "#{number+31}", seller_id: 1, name: "おもちゃ・ホビー・グッズ", description: "[激レア] 良品です!!", category_id: 715, size: 0, brand_id: 8, condition: 0, postage_burden: 0, sending_method: 0, prefecture_from: 0, shipping_date: 0, price: 999)
 end
   
 10.times do |number|
