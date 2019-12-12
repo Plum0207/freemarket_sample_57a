2f85f3bd6b571633f54562648636a50acfe6a457
Yoshi Tadokoro <yossher@gmail.com>
2019-12-12 12:19:23 +0900

    items_controller及びindexの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,15 @@
 class ItemsController < ApplicationController
   def index
     #condition等その他のオプションは後で追記出来ます（例）@lady_items = Item.where(category: 1.14..215).where(condition: 0).order('created_at DESC').limit(10).where.not(condition: 1)
-    #カテゴリー名の脇に書いてある数字（14..215）はそのカテゴリーに当てはまる番号ですが今は上手く書けないので保留
-    #レディース + 14..215
-    @lady_items = Item.where(category: 1).order('created_at DESC').limit(10)
-    #メンズ + 216..360
-    @man_items = Item.where(category: 2).order('created_at DESC').limit(10)
-    #家電 + 906..989
-    @appliance_items = Item.where(category: 8).order('created_at DESC').limit(10)
-    #おもちゃ・ホビー・グッズ + 695..806
-    @hobby_items = Item.where(category: 6).order('created_at DESC').limit(10)
+    
+    #レディース
+    @lady_items = Item.where(category: 14..215).order('created_at DESC').limit(10)
+    #メンズ
+    @man_items = Item.where(category: 216..360).order('created_at DESC').limit(10)
+    #家電
+    @appliance_items = Item.where(category: 906..989).order('created_at DESC').limit(10)
+    #おもちゃ・ホビー・グッズ
+    @hobby_items = Item.where(category: 695..806).order('created_at DESC').limit(10)
   
     #シャネル
     @channel_items = Item.where(brand: 1).order('created_at DESC').limit(10)
