200b012c9887e701dedb9f7fe66fb031c85d4712
Yoshi Tadokoro <yossher@gmail.com>
2019-12-21 16:48:34 +0900

    items_controllerの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,13 @@
 class ItemsController < ApplicationController
   def index
-    #condition等その他のオプションは後で追記出来ます（例）@lady_items = Item.where(category: 1.14..215).where(condition: 0).order('created_at DESC').limit(10).where.not(condition: 1)
-    
     #レディース
-    @lady_items = Item.where(category: 14..215).order('created_at DESC').limit(10)
+    @lady_items = Item.where(category: 33..215).order('created_at DESC').limit(10)
     #メンズ
-    @man_items = Item.where(category: 216..360).order('created_at DESC').limit(10)
+    @man_items = Item.where(category: 230..360).order('created_at DESC').limit(10)
     #家電
-    @appliance_items = Item.where(category: 906..989).order('created_at DESC').limit(10)
+    @appliance_items = Item.where(category: 916..989).order('created_at DESC').limit(10)
     #おもちゃ・ホビー・グッズ
-    @hobby_items = Item.where(category: 695..806).order('created_at DESC').limit(10)
+    @hobby_items = Item.where(category: 705..806).order('created_at DESC').limit(10)
   
     #シャネル
     @channel_items = Item.where(brand: 1).order('created_at DESC').limit(10)
