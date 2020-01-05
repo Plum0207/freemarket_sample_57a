d03d160192cd08118a9e81430ec98afdb4c00c1f
Plum0207 <lapine.blanche.27@gmail.com>
2020-01-05 14:09:21 +0900

    Define variables in photoshow.js

---
+++ app/assets/javascripts/photoshow.js
@@ -0,0 +1,9 @@
+
+// items/show.htmal.hamlのphotoshow
+
+$(function(){
+  let $container = $('.photoshow'),
+      $photoGroup = $container.find('.photoshow-photos'),
+      $thumnail = $container.find('.thumnail-photo');
+
+});
