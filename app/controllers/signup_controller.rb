20b9e54055c576f0860be9933000e87890dafce8
kanato4 <kntykt@icloud.com>
2020-01-18 19:26:34 +0900

    create SMS function
    ショートメッセージを送り、コードが入力されればユーザ登録の続きに進めるように実装。

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,6 +1,10 @@
 class SignupController < ApplicationController
+  include RandomValue
+
   before_action :save_user_info_to_session, only: :user_tel
-  before_action :save_user_tel_to_session, only: :user_address
+  before_action :save_user_tel_to_session, only: :user_tel_verification
+  before_action :send_sms_code, only: :user_tel_verification
+  before_action :check_sms_code, only: :user_address
   before_action :save_user_address_to_session, only: :create
 
   def user_info
@@ -24,6 +28,37 @@ class SignupController < ApplicationController
     @user = User.new(session[:user_params])
     @user.build_user_address(session[:user_address_attributes_after_user_tel])
     render '/signup/user_tel' unless @user.valid?
+  end
+
+  def send_sms_code
+    @user = User.new(session[:user_params])
+    @user.build_user_address(session[:user_address_attributes_after_user_tel])
+    send_phone_number      =   PhonyRails.normalize_number session[:user_address_attributes_after_user_tel][:telephone], country_code:'JP'
+    session[:secure_code]  =   random_number_generator(4)
+    begin
+      client = Twilio::REST::Client.new
+      result = client.messages.create(
+        from: ENV["TWILIO_PHONE_NUMBER"],
+        to:   send_phone_number,
+        body: "認証番号：#{session[:secure_code]}"
+      )
+    rescue Twilio::REST::RestError => e
+      @messages = "エラーコード[#{e.code}] ：”#{e.message}”"
+      render '/signup/user_tel'
+    end
+  end
+
+  def user_tel_verification
+    @user = User.new(session[:user_params])
+    @user.build_user_address
+  end
+
+  def check_sms_code
+    session[:input_code] = user_params[:user_address_attributes]
+    @user = User.new(session[:user_params])
+    @user.build_user_address(session[:user_address_attributes_after_user_tel])
+    redirect_to action: :user_tel_verification unless session[:input_code].present?
+    render '/signup/user_tel_verification' and return unless session[:secure_code] == session[:input_code][:telephone]
   end
 
   def user_address
