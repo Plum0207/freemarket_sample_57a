0f21f47da9a2cccbb8bc68567c766a4f53daf821
ddaawwaa <nlghtnmnt3106@gmail.com>
2020-01-27 00:39:12 +0900

    Add twilio APIkey for production

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,5 +1,10 @@
 require 'twilio-ruby'
 Twilio.configure do |config|
-  config.account_sid = ENV['TWILIO_ACCOUNT_SID']
-  config.auth_token = ENV['TWILIO_AUTH_TOKEN']
+  if Rails.env.development? || Rails.env.test?
+    config.account_sid = ENV['TWILIO_ACCOUNT_SID']
+    config.auth_token = ENV['TWILIO_AUTH_TOKEN']
+  else
+    config.account_sid = Rails.application.credentials.twilio[:TWILIO_ACCOUNT_SID]
+    config.auth_token = Rails.application.credentials.twilio[:TWILIO_AUTH_TOKEN]
+  end
 end
